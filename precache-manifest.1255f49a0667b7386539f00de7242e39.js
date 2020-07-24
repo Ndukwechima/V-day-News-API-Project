c69a5d66fe6f8c5afbc1f84d10c65ff394a910ca
Barnabas <Barnabas-chimaobi>
2020-07-24 21:34:43 +0100

    Updates

---
+++ precache-manifest.1255f49a0667b7386539f00de7242e39.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae8b40bc15a1f75c9069ab1c59b0113",
+    "url": "/V-day-News-API-Project/index.html"
+  },
+  {
+    "revision": "e3e07b197ab365e87545",
+    "url": "/V-day-News-API-Project/static/css/main.29b6f6ba.chunk.css"
+  },
+  {
+    "revision": "1224441c874407385209",
+    "url": "/V-day-News-API-Project/static/js/2.01d75208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/V-day-News-API-Project/static/js/2.01d75208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e07b197ab365e87545",
+    "url": "/V-day-News-API-Project/static/js/main.ba4c22be.chunk.js"
+  },
+  {
+    "revision": "3a97cac63957a0088839",
+    "url": "/V-day-News-API-Project/static/js/runtime-main.ca10f198.js"
+  },
+  {
+    "revision": "128bb311fed4426cb7f7e312120c401a",
+    "url": "/V-day-News-API-Project/static/media/1.128bb311.jpg"
+  },
+  {
+    "revision": "67722d2ab9f59e692184f0a466431ecb",
+    "url": "/V-day-News-API-Project/static/media/Healthy-Fruits.67722d2a.jpg"
+  },
+  {
+    "revision": "0533f6e959fc312b2a91c847c8cd4747",
+    "url": "/V-day-News-API-Project/static/media/america.0533f6e9.jpg"
+  },
+  {
+    "revision": "61e28ebe81baaed2ce5bc29422aa0867",
+    "url": "/V-day-News-API-Project/static/media/education.61e28ebe.jpg"
+  },
+  {
+    "revision": "6621337c34d72b4503c22b3744e6d40b",
+    "url": "/V-day-News-API-Project/static/media/loading3.6621337c.gif"
+  },
+  {
+    "revision": "1af601bb8830c9a7bfa2cc25bc23d99d",
+    "url": "/V-day-News-API-Project/static/media/messi.1af601bb.jpeg"
+  },
+  {
+    "revision": "96a380eb2ee1966c993b39579443b5cd",
+    "url": "/V-day-News-API-Project/static/media/pogba.96a380eb.jpg"
+  },
+  {
+    "revision": "68dcd7b696b1a4c491e73c876b7a097a",
+    "url": "/V-day-News-API-Project/static/media/scitech.68dcd7b6.jpg"
+  },
+  {
+    "revision": "e17c059f999c5f0dfb0e3c2ea92aca14",
+    "url": "/V-day-News-API-Project/static/media/too.e17c059f.png"
+  },
+  {
+    "revision": "47dd0789b371c3c026ed22b3c2aead38",
+    "url": "/V-day-News-API-Project/static/media/trump.47dd0789.jpg"
+  },
+  {
+    "revision": "7c49a0feb8231105c4ac36b70ab4fe03",
+    "url": "/V-day-News-API-Project/static/media/water.7c49a0fe.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.441df4ac75e30bbe1678a4bc38ffa76b.js"
+  "/V-day-News-API-Project/precache-manifest.1255f49a0667b7386539f00de7242e39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/V-day-News-API-Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
